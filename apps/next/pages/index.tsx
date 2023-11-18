c49cdcd6f55f8ee753719f3b6680391b3f9ee74c
benjamin <benschac@gmail.com>
2023-11-18 10:44:34 -0500

    add blank components

--- apps/next/pages/index.tsx
+++ apps/next/pages/index.tsx
@@ -1,5 +1,8 @@
 import { HomeScreen } from 'app/features/home/screen'
+import { About, Experience, Hero, Tech, Works, Feedbacks, Navbar, Contact } from '../components'
 import Head from 'next/head'
+import { YStack } from '@my/ui'
+import Stars from 'components/canvas/Stars'
 
 export default function Page() {
   return (
@@ -7,7 +10,19 @@ export default function Page() {
       <Head>
         <title>Home</title>
       </Head>
-      <HomeScreen />
+      <Navbar />
+      <Hero />
+      <YStack>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <YStack pos="relative" zi={0}>
+          <Contact />
+          <Stars />
+        </YStack>
+      </YStack>
     </>
   )
 }
